97c2c10ca77ca5129b8be682c9c22dc0b064810d
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-04-17 08:45:17 +0200

    espacemembres: Login et logout

--- php/espacemembres/account.php
+++ php/espacemembres/account.php
@@ -1,11 +1,9 @@
 <?php
-session_start();
 require 'inc/functions.php';
+logged_only();
+require 'inc/header.php';
 ?>
-<?php require 'inc/header.php'; ?>
 
-<h1>Votre compte</h1>
-
-<?php debug($_SESSION); ?>
+<h1>Bonjour <?= $_SESSION['auth']->username ?></h1>
 
 <?php require 'inc/footer.php'; ?>
---
+++ php/espacemembres/login.php
@@ -0,0 +1,36 @@
+<?php
+if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
+    require_once 'inc/connexionDb.php';
+    $req = $pdo->prepare('SELECT * FROM membres WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
+    $req->execute(['username' => $_POST['username']]);
+    $user = $req->fetch();
+    if($user == null){
+        $_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrecte';
+    } elseif (password_verify($_POST['password'], $user->password)){
+        session_start();
+        $_SESSION['auth'] = $user;
+        $_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
+        header('Location: account.php');
+        exit();
+    } else {
+        $_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrecte';
+    }
+}
+?>
+<?php require 'inc/header.php'; ?>
+
+<h1>Se connecter</h1>
+
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="">Pseudo ou email</label>
+        <input type="text" name="username" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="">Mot de passe</label>
+        <input type="password" name="password" class="form-control" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+</form>
+
+<?php require 'inc/footer.php'; ?>
