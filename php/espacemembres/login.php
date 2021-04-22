6a205d0015a8b8970ce6dd358984ef95767b0f1d
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-04-22 14:50:33 +0200

    espacemembres: Se souvenir de moi

--- php/espacemembres/login.php
+++ php/espacemembres/login.php
@@ -1,6 +1,12 @@
 <?php require 'inc/header.php'; ?>
 
 <?php
+require_once 'inc/functions.php';
+reconnect_from_cookie();
+if(isset($_SESSION['auth'])){
+    header('Location: account.php');
+    exit();
+}
 if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
     require_once 'inc/connexionDb.php';
     $req = $pdo->prepare('SELECT * FROM membres WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
@@ -12,6 +18,11 @@ if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
         session_start();
         $_SESSION['auth'] = $user;
         $_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
+        if($_POST['remember']){
+            $remember_token = str_random(250);
+            $pdo->prepare('UPDATE membres SET remember_token = ? WHERE id = ?')->execute([$remember_token, $user->id]);
+            setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id . 'momococo'), time() + 60 * 60 * 24 * 7);
+        }
         header('Location: account.php');
         exit();
     } else {
@@ -30,6 +41,11 @@ if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
     <div class="form-group">
         <label for="">Mot de passe <a href="forget.php">(J'ai oublié mon mot de passe)</a></label>
         <input type="password" name="password" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label>
+            <input type="checkbox" name="remember" value="1"> Se souvenir de moi
+        </label>
     </div>
     <button type="submit" class="btn btn-primary">Se connecter</button>
 </form>
